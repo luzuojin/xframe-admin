bd91df359cd573d935f704c904b16741dd25131e
luzj <luzj@hoolai.com>
2019-11-04 11:59:51 +0800

    remove unused

--- src/main/resources/log_init.sql
+++ src/main/resources/log_init.sql
@@ -1,7 +1,4 @@
-CREATE TABLE `t_role` (
+CREATE TABLE `t_op_log` (
   `Id` int(11) NOT NULL,
-  `Name` varchar(64) NOT NULL,
-  `Authorities` varchar(1024) NOT NULL,
-  `ReadOnly` tinyint(1) NOT NULL,
   PRIMARY KEY (`Id`)
 ) DEFAULT CHARSET=utf8;
