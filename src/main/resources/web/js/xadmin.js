e47f28f5b8945dd789d0a534f18d1166492e406b
luzj <luzj@hoolai.com>
2019-11-08 19:29:57 +0800

    web res

---
+++ src/main/resources/web/js/xadmin.js
@@ -0,0 +1,358 @@
+String.prototype.format = function() {
+    var str = this;
+    for (let key in arguments) {
+        let reg = new RegExp("({)" + key + "(})", "g");
+        str = str.replace(reg, arguments[key]);
+    }
+    return str;
+}
+
+var xurl = 'http://127.0.0.1:8001'
+var xpaths = {
+    summary: "basic/summary",
+    xenum  : "basic/enum",
+}
+
+var xuserseg = {//用户登录/修改密码...
+    spath: "basic",
+    path:  "profile",
+    name:  "用户",
+    columns: [
+      {key:"name",hint:"用户名",type:xTypes._text,show:13},
+      {key:"passw",hint:"密码",type:xTypes._pass,show:15}
+    ]
+};
+
+var opTypes = {
+    qry: 1,
+    add: 2,
+    edt: 3,
+    del: 4
+}
+
+var xcolumn = {
+    list: function(c){return (c.show & 1) > 0;},
+    edit: function(c){return (c.show & 2) > 0;},
+    add : function(c){return (c.show & 4) > 0;},
+    edel: function(c){return (c.show & 8) > 0;},
+}
+
+var xuser;
+var xsummary;
+var xlatestSeg;
+var xlatestOp;
+
+function xenum(key) {
+return $.parseJSON($.ajax({
+        type: "GET",
+        url: "{0}/{1}?key={2}".format(xurl, xpaths.xenum, key),
+        cache: false,
+        async: false
+    }).responseText).data;
+}
+
+function doLogin() {
+    let op = {
+        name: "登录",
+        opType: opTypes.add
+    };
+    showDialog(xuserseg, op, undefined, onLogin);
+}
+
+function onLogin(data) {
+    showUser(data);
+    showSummary();
+}
+
+function showUser(user) {
+    xuser = user;
+    $('#xuser').empty();
+    $('#xuser').append(x.userhtm(user.name));
+
+    $('#xboxhead').append('<h3>Welcome</h3>');
+
+    xclick($('#xuser_logout'), function(){
+        doPost(segpath(xuserseg), opTypes.del, {name: xuser.name, passw: ''}, function(resp){
+            xuser = undefined;
+            location.reload();
+        });
+    });
+
+    xclick($('#xuser_profile'), function(){
+        let op = {
+            name: "修改密码",
+            opType: opTypes.edt
+        };
+        let model = {
+            name: xuser.name,
+            passw: ''
+        }
+        showDialog(xuserseg, op, model, function(){});
+    });
+}
+
+function showSummary() {
+    doGet(xpaths.summary, showSiderbar);
+}
+
+function showSiderbar(data) {
+    xsummary = data;
+    for(let chapter of data.chapters){
+        $('#xsiderbar').append(x.chapterhtm(chapter));
+        for(let segment of chapter.segments){//二级菜单
+            segment.spath = chapter.path;
+            x.chapterdom(chapter).append(x.segmenthtm(segment));
+            xclick(x.segmentdom(segment), showDetailFunc(segment));
+        }
+    }
+}
+
+function queryDatasFunc(segment, op) {
+    return function() {
+        let params = {};
+        for(let input of op.inputs) {
+            params[input.key] = x.qryInputDom(input.key).val();
+        }
+        doGet('{0}?{1}'.format(segpath(segment), ($.param(params))), function(data){showDetailBody(segment, data);});
+    }
+}
+
+function showDetailFunc(segment) {
+    return function() {
+        if(xlatestSeg)
+           x.segmentdom(xlatestSeg).removeClass('active'); 
+        xlatestSeg = segment;
+        x.segmentdom(segment).addClass('active');
+
+        if(segment.listable) {
+            doGet('{0}/list'.format(segpath(segment)), function(data){showDetail(segment, data);});
+        } else {
+            showDetail(segment, []);
+        }
+    };
+}
+
+var _op;
+function showDetail(segment, data) {
+    $('#xthead').empty();
+    $('#xboxhead').empty();
+
+    _op = false;
+
+    let _tr = 0;
+    //box head
+    for(let op of segment.options) {
+        if(op.opType == opTypes.qry) {//only query use inputs
+            for(let input of op.inputs) {
+                x.addQryInput($('#xboxhead'), input);
+            }
+            $('#xboxhead').append(x.qryBtn.format(segment.path, _tr, op.name));
+            xclick(x.qryBtnDom(segment.path, _tr), queryDatasFunc(segment, op)) 
+        }
+        if(op.opType == opTypes.add) {
+            $('#xboxhead').append(x.addBtn.format(segment.path, _tr, op.name));
+            xclick(x.addBtnDom(segment.path, _tr), showDialogFunc(segment, op));
+        }
+        if(op.opType == opTypes.edt) _op = true;
+        if(op.opType == opTypes.del) _op = true;
+    }
+
+    //box body --> table
+    //table head
+    $('#xthead').append($(x.tabletr.format(_tr)))
+    for(let column of segment.columns){
+        if(xcolumn.list(column)) {
+            x.tabletrDom(_tr).append($(x.tabletd.format(_tr, 0, column.hint)));
+        }
+    }
+    if(_op) {//options td head
+        x.tabletrDom(_tr).append($(x.tabletd.format(_tr, 0, "Options")));    
+    }
+
+    xmodel.set(segment, data);
+    //table body
+    showDetailBody(segment, xmodel.datas);
+}
+
+function showDetailBody(segment, data) {
+    $('#xtbody').empty();
+
+    let _tr = 0;
+    //table body
+    for(let model of data) {
+        model._id = (++ _tr);
+        $('#xtbody').append($(x.tabletr.format(_tr)))
+        var _td = 0;
+        for(let column of segment.columns){
+            if(xcolumn.list(column)) {
+                x.tabletrDom(_tr).append($(x.tabletd.format(_tr, (++_td), model[column.key])));
+            }
+        }
+        //options td
+        if(_op) {
+            x.tabletrDom(_tr).append($(x.tabletd.format(_tr, (++_td), '')));
+            for(let op of segment.options) {
+                if(op.opType == opTypes.edt) {
+                    x.tabletdDom(_tr, _td).append(x.edtBtn.format(segment.path, _tr, op.name));
+                    xclick(x.edtBtnDom(segment.path, _tr), showDialogFunc(segment, op, model));
+                }
+                if(op.opType == opTypes.del) {
+                    x.tabletdDom(_tr, _td).append(x.delBtn.format(segment.path, _tr, op.name));
+                    xclick(x.delBtnDom(segment.path, _tr), showDialogFunc(segment, op, model));
+                }
+            }
+        }
+    }
+}
+
+
+function showDialogFunc(segment, op, model=undefined) {
+    return function() {
+        showDialog(segment, op, model, function(data){
+            if(op.opType == opTypes.qry || Array.isArray(data)) {
+                xmodel.set(segment, data);
+            }else{
+                if(op.opType == opTypes.add) xmodel.add(data);
+                if(op.opType == opTypes.edt) xmodel.edt(data);
+                if(op.opType == opTypes.del) xmodel.del(data);
+            }
+            showDetailBody(segment, xmodel.datas);
+        });
+    };
+}
+
+
+function dialogTitle(segment, op) {
+    return '{0}&nbsp;/&nbsp;{1}'.format(segment.name, op.name)
+}
+
+function showDialog(segment, op, model, func) {
+    $('#xdialog_title').empty();
+    $('#xdialog_form').empty();
+
+    $('#xdialog_title').append(dialogTitle(segment, op))
+    for(let column of segment.columns){
+        if(op.opType == opTypes.add && !xcolumn.add(column)) continue;
+        if(op.opType >= opTypes.edt && !xcolumn.edel(column)) continue;
+        let val = model ? model[column.key] : '';
+        x.addDlgInput($('#xdialog_form'), column, segment.path, val);
+        if (op.opType == opTypes.del || (model && !xcolumn.edit(column))) {
+            x.dlgInputDom(segment.path, column.key).attr("disabled", true);
+        }
+    }
+    xclick($('#xdialog_submit'), function(){submitDialog(segment, op, func);})
+    modalShow();
+}
+
+function submitDialog(segment, op, func) {
+    var model = {};
+    for(let column of segment.columns) {
+        let key = column.key;
+        model[key] = x.dlgInputDom(segment.path, key).val();
+        if(column.type==xTypes._pass) model[key]=$.md5(model[key]);
+    }
+    xlatestOp = op;
+    doPost(segpath(segment), op.opType, model,
+        function(resp) {
+            modalHide();
+            func(resp);
+        }
+    );
+}
+
+function modalShow() {
+    $('#xdialog').modal('show');
+}
+
+function modalHide() {
+    $('#xdialog').modal('hide');
+}
+
+function segpath(segment) {
+    return '{0}/{1}'.format(segment.spath, segment.path);
+}
+
+function xtoken() {
+    return xuser ? xuser.token : '';
+}
+
+httpTypes = ['_', 'get', 'post', 'put', 'delete']
+function doPost(path, opType, data, func) {
+    $.ajax({
+        type: httpTypes[opType],
+        url: '{0}/{1}'.format(xurl, path),
+        data: JSON.stringify(data),
+        headers: {"x-token": xtoken()},
+        dataType: 'json',
+        success: doResp(func)
+    });
+}
+
+function doGet(path, func) {
+    console.log(xtoken());
+    $.ajax({
+        type: 'get',
+        url: '{0}/{1}'.format(xurl, path),
+        headers: {"x-token": xtoken()},
+        dataType: 'json',
+        success: doResp(func)
+    });
+}
+
+var xtoast = {
+    show: function(type, msg) {
+            Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1500
+        }).fire({
+            type: type,
+            title: msg
+        });
+    },
+    succ : function(msg) {this.show('success', msg)},
+    error: function(msg) {this.show('error', msg)}
+}
+
+function doResp(func) {
+    return function(resp) {
+        if(resp.status == -1) {
+            if(resp.data) {
+                xtoast.error(resp.data);
+            } else {
+                xtoast.error('{0} 失败'.format(xlatestOp.name));
+            }
+        } else if(xlatestOp) {
+            xtoast.succ('{0} 成功'.format(xlatestOp.name));
+            func(resp.data);
+        } else {
+            func(resp.data);
+        }
+        xlatestOp = undefined;
+    }
+}
+
+function xclick(btn, func) {
+    btn.off('click');
+    btn.click(func);
+}
+
+function initial() {
+// popup dialog keypress listening
+    $('#xdialog').on('hide.bs.modal ', function(){
+        $(document).off('keyup');
+    });
+    $('#xdialog').on('shown.bs.modal', function(){
+        $('#xdialog').focus();
+        $(document).on('keyup', function(e){
+            if(e.keyCode==13) {$("#xdialog_submit").trigger('click');}
+            if(e.keyCode==27) {$("#xdialog_close").trigger('click');}
+        });
+    });
+}
+
+$(function () {
+    initial();
+    doLogin();
+});
---
+++ src/main/resources/web/js/xstyles.js
@@ -0,0 +1,126 @@
+//input types
+var xTypes = {
+    _text: 0,
+    _bool: 1,
+    _enum: 2,
+    _time: 3,
+    _area: 4,
+    _pass: 9,
+    _mult: 20,//multi enum select
+}
+
+var x = {
+user: `<i class="fas fa-user"></i> {0}`,
+userhtm: function(name){return this.user.format(name);},
+
+chapter: `
+        <li class="nav-item has-treeview">
+            <a class="nav-link" style="background-color:rgba(255,255,255,.05) !important;" href="javascript:void(0);">
+              <i class="nav-icon fab fa-gg"/>
+              <p>
+                {0}
+                <i class="right fas fa-angle-left"/>
+              </p>
+            </a>
+            <ul id="chapter_{1}" class="nav nav-treeview"/>
+        </li>
+        `,
+chapterhtm: function(chapter){return this.chapter.format(chapter.name, chapter.path)},
+//chapter or segment
+chapterdom: function(data){return $('#chapter_{0}'.format(data.spath?data.spath : data.path));},
+
+segment: `
+        <li class="nav-item">
+            <a id="seg_{0}_{1}" class="nav-link" href="javascript:void(0);">
+              <i class="fas fa-paperclip"/>
+              <p>{2}</p>
+            </a>
+        </li>
+        `,
+segmenthtm: function(segment){return this.segment.format(segment.spath, segment.path, segment.name);},
+segmentdom: function(segment){return $('#seg_{0}_{1}'.format(segment.spath, segment.path));},
+
+tabletd: `<td id='xtd_{0}_{1}'>{2}</td>`,
+tabletr: `<tr id='xtr_{0}'/>`,
+tabletdDom: function(tr, td){return $('#xtd_{0}_{1}'.format(tr, td))},
+tabletrDom: function(tr){return $('#xtr_{0}'.format(tr))},
+
+delBtn: `<button id="delbtn_{0}_{1}" type="button" class="btn-sm btn-outline-danger">{2}</button>`,
+edtBtn: `<button id="edtbtn_{0}_{1}" type="button" class="btn-sm btn-outline-info" style="margin-right:5px">{2}</button>`,
+qryBtn: `<button id="qrybtn_{0}_{1}" type="button" class="btn bg-gradient-info float-left" style="margin-left:7.5px;margin-right:7.5px;">{2}</button>`,
+addBtn: `<button id="addbtn_{0}_{1}" type="button" class="btn bg-gradient-success float-right" style="margin-left:7.5px;margin-right:7.5px;">{2}</button>`,
+delBtnDom: function(path, tr){return $('#delbtn_{0}_{1}'.format(path, tr))},
+edtBtnDom: function(path, tr){return $('#edtbtn_{0}_{1}'.format(path, tr))},
+qryBtnDom: function(path, tr){return $('#qrybtn_{0}_{1}'.format(path, tr))},
+addBtnDom: function(path, tr){return $('#addbtn_{0}_{1}'.format(path, tr))},
+
+qryInputDom: function(k){return $('#xqry_{0}'.format(k))},
+qryText: `<div class="col-sm-2 float-left"><input id="xqry_{0}" class="form-control" type="text" placeholder="{1}" autocomplete="off"></div>`,
+qryEnum: `<div class="col-sm-2 float-left"><select id="xqry_{0}" class="form-control select2bs4" data-placeholder="{1}" style="width:100%"><option/></select></div>`,
+
+addQryInput: function(parent, xinput) {
+    if(xinput.type == xTypes._enum) {
+        parent.append(this.qryEnum.format(xinput.key, xinput.hint));
+        this.select2(this.qryInputDom(xinput.key), xinput);
+    } else {
+        parent.append(this.qryText.format(xinput.key, xinput.hint));
+        if(xinput.type==xTypes._time) this.datepicker(this.qryInputDom(xinput.key));
+    }
+},
+
+dlgColumn: `
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="dinput_{0}_{1}"><p class="float-right">{2}</p></label>
+          <div class="col-sm-10">{3}</div>
+        </div>
+        `,
+dlgText:`<input id="dinput_{0}_{1}" class="form-control" placeholder="{2}" value="{3}" type="{4}">`,
+dlgEnum:`<select id="dinput_{0}_{1}" class="form-control select2" data-placeholder="{2}" style="width:100%"></select>`,
+dlgBool:`
+        <div class="form-control custom-control custom-switch custom-switch-on-primary">
+          <input id="dinput_{0}_{1}" type="checkbox" class="custom-control-input" value="false">
+          <label class="custom-control-label" for="dinput_{0}_{1}" style="margin-left:7.5px;"/>
+        </div>
+        `,
+dlgInputDom: function(idkey, inputkey){return $('#dinput_{0}_{1}'.format(idkey, inputkey));},
+
+addDlgInput: function(parent, xinput, idkey, value) {
+    if(xinput.type==xTypes._enum || xinput.type==xTypes._mult) {
+        let _col = this.dlgEnum.format(idkey, xinput.key, xinput.hint);
+        parent.append(this.dlgColumn.format(idkey, xinput.key, xinput.hint, _col));
+        this.select2(this.dlgInputDom(idkey, xinput.key), xinput);
+        if(value) this.dlgInputDom(idkey, xinput.key).val(value).trigger('change');
+    } else if(xinput.type==xTypes._bool) {
+        let _col = this.dlgBool.format(idkey, xinput.key);
+        parent.append(this.dlgColumn.format(idkey, xinput.key, xinput.hint, _col));
+        let ckbox = this.dlgInputDom(idkey, xinput.key);
+        ckbox.change(function (){ckbox.val(this.checked);});
+        if(value) ckbox.attr('checked', value);
+    } else {
+        let _type = (xinput.type==xTypes._pass) ? 'password' : 'text';
+        let _col = this.dlgText.format(idkey, xinput.key, xinput.hint, value, _type);
+        parent.append(this.dlgColumn.format(idkey, xinput.key, xinput.hint, _col));
+        if(xinput.type==xTypes._time) this.datepicker(this.dlgInputDom(idkey, xinput.key));
+    }
+},
+
+datepicker: function(e) {
+    e.datepicker({
+        format: "yyyy-mm-dd",
+        autoclose: true,
+        isRTL: false,
+        language: "zh-CN"
+    });
+},
+
+select2: function(e, xinput) {
+    let d = xenum(xinput.enumKey);
+    e.select2({
+        theme: 'bootstrap4',
+        data: d,
+        multiple: xinput.type==xTypes._mult,
+        minimumResultsForSearch: 10
+    });
+},
+
+};
