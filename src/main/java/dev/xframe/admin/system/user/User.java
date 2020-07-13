752669149636663e1d94692d0cf1708558ee1f52
luzj <luzj@hoolai.com>
2020-07-13 17:40:49 +0800

    column type bug fix

--- src/main/java/dev/xframe/admin/system/user/User.java
+++ src/main/java/dev/xframe/admin/system/user/User.java
@@ -19,7 +19,7 @@ public class User {
 	private int[] roles;
 	@XColumn(value="角色", show=XColumn.list)
 	private Object[] rolesDesc;
-	@XColumn(value="创建时间", show=XColumn.list_edel, type=XColumn.type_time)
+	@XColumn(value="创建时间", show=XColumn.list_edel, type=XColumn.type_datetime)
 	private Timestamp ctime;
 	
 	public User() {
