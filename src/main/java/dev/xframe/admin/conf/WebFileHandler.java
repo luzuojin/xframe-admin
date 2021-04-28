75feb7c69c7977a3714a150f5f19c42826be91d0
luzj <luzj@hulai.com>
2021-04-28 15:55:14 +0800

    change icon and title

--- src/main/java/dev/xframe/admin/conf/WebFileHandler.java
+++ src/main/java/dev/xframe/admin/conf/WebFileHandler.java
@@ -1,7 +1,10 @@
 package dev.xframe.admin.conf;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -14,17 +17,26 @@ import java.util.jar.JarFile;
 import dev.xframe.admin.system.auth.AuthContext;
 import dev.xframe.http.Request;
 import dev.xframe.http.Response;
+import dev.xframe.http.response.ContentType;
 import dev.xframe.http.response.FileResponse;
 import dev.xframe.http.service.Service;
 import dev.xframe.http.service.ServiceContext;
 import dev.xframe.inject.Bean;
 import dev.xframe.inject.Eventual;
 import dev.xframe.inject.Inject;
+import dev.xframe.utils.XCaught;
 import dev.xframe.utils.XPaths;
+import dev.xframe.utils.XProperties;
 
 
 @Bean
 public class WebFileHandler implements Eventual, Service {
+    
+    private static final String TitlePropKey  = "xframe.admin.titile";
+    private static final String IconPropKey   = "xframe.admin.icon";
+    private static final String IndexFileName = "index.html";
+    private static final String DefaultTitle  = "XFrameAdmin";
+    private static final String DefaultIcon   = "xframe.png";
     
     @Inject
     private ServiceContext serviceCtx;
@@ -37,24 +49,53 @@ public class WebFileHandler implements Eventual, Service {
     
     private String root;
     
+    
+    private Response index;
+    
     @Override
     public Response exec(Request req) {
         return func.apply(req);
     }
     
+    private boolean isIndexHtml(Request req) {
+        return IndexFileName.equals(req.xpath());
+    }
+
+    private Response indexResp() {
+        return index;
+    }
+    
     @Override
     public void eventuate() {
-        String path = XPaths.toPath(WebFileHandler.class.getProtectionDomain().getCodeSource().getLocation());
-        String xdir = "web/";
-        if(path.endsWith(".jar")) {
-            this.root = xdir;
-            listRelativizeJarFiles(path, xdir).forEach(this::makeHandler);
-            this.func = this::makeRespFromClassPath;
-        } else {
-            this.root = new File(path, xdir).getPath();
-            XPaths.listRelativizeFiles(root).forEach(this::makeHandler);
-            this.func = this::makeRespFromDirectory;
+        try {
+            String path = XPaths.toPath(WebFileHandler.class.getProtectionDomain().getCodeSource().getLocation());
+            String xdir = "web/";
+            if(path.endsWith(".jar")) {
+                this.root = xdir;
+                listRelativizeJarFiles(path, xdir).forEach(this::makeHandler);
+                this.func = this::makeRespFromClassPath;
+                this.index = new FileResponse.Binary(ContentType.HTML, readIndexHtml(WebFileHandler.class.getClassLoader().getResourceAsStream(IndexFileName)));
+            } else {
+                this.root = new File(path, xdir).getPath();
+                XPaths.listRelativizeFiles(root).forEach(this::makeHandler);
+                this.func = this::makeRespFromDirectory;
+                this.index = new FileResponse.Binary(ContentType.HTML, readIndexHtml(new FileInputStream(new File(root, IndexFileName))));
+            }
+        } catch (Exception e) {
+            XCaught.throwException(e);
         }
+    }
+
+    private byte[] readIndexHtml(InputStream input) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int b;
+        while((b = input.read()) != -1) {
+            out.write(b);
+        }
+        String content = out.toString();
+        content = content.replace(DefaultTitle, XProperties.get(TitlePropKey, DefaultTitle));
+        content = content.replace(DefaultIcon, XProperties.get(IconPropKey, DefaultIcon));
+        return content.getBytes();
     }
 
     private void makeHandler(String p) {
@@ -64,6 +105,9 @@ public class WebFileHandler implements Eventual, Service {
     }
     
     private Response makeRespFromDirectory(Request req) {
+        if(isIndexHtml(req)) {
+            return indexResp();
+        }
         return new FileResponse.Sys(getReqFile(req));
     }
 
@@ -72,6 +116,9 @@ public class WebFileHandler implements Eventual, Service {
     }
     
     private Response makeRespFromClassPath(Request req) {
+        if(isIndexHtml(req)) {
+            return indexResp();
+        }
     	String path = getReqFile(req).getPath();
     	Response resp = caches.get(path);
     	if(resp == null) {
