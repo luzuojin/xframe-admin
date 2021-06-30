b51cd68544b3b7145452aa8c56754314bc517992
luzj <luzj@hoolai.com>
2021-06-30 22:56:40 +0800

    hint first to uppercase

--- src/main/java/dev/xframe/admin/view/Column.java
+++ src/main/java/dev/xframe/admin/view/Column.java
@@ -3,90 +3,82 @@ package dev.xframe.admin.view;
 import dev.xframe.utils.XStrings;
 
 public class Column {
-	
-	private int type;
-	private String key;
-	private String hint;
-	private String enumKey;
-	
-	private int show;
-	private boolean primary;
-	private boolean collapse;
+    private int type;
+    private String key;
+    private String hint;
+    private String enumKey;
+
+    private int show;
+    private boolean primary;
+    private boolean collapse;
     private boolean required;
-	
-	public Column(String key) {
-		this(key, key, XColumn.type_text, "", XColumn.full, false, false, false);
-	}
-	public Column(String key, XColumn xc, Class<?> jType) {
-		this(key, xc, byJavaType(xc.type(), jType));
-	}
-	static int byJavaType(int xtype, Class<?> jtype) {
-		if(xtype == 0 && (jtype == boolean.class || jtype == Boolean.class)) {
-			return XColumn.type_bool;
-		}
-		return xtype;
-	}
-	public Column(String key, XColumn xc, int xcType) {
-		this(key, XStrings.orElse(xc.value(), key), xcType, xc.enumKey(), xc.show(), xc.primary(), xc.collapse(), xc.required());
-	}
-	public Column(String key, String hint, int type, String enumKey, int show, boolean primary, boolean collapse, boolean required) {
-		this.key = key;
-		this.hint = hint;
-		this.type = type;
-		this.enumKey = enumKey;
-		this.show = show;
-		this.primary = primary;
-		this.collapse = collapse;
-		this.required = required;
-		
-		if(!XStrings.isEmpty(enumKey) && this.type == 0)
-			this.type = XColumn.type_enum;
-	}
 
-	public int getShow() {
-		return show;
-	}
+    static int byJavaType(int xtype, Class<?> jtype) {
+        if(xtype == 0 && (jtype == boolean.class || jtype == Boolean.class)) {
+            return XColumn.type_bool;
+        }
+        return xtype;
+    }
+    static String firstToUpperCase(String key) {
+        return String.valueOf(key.charAt(0)).toUpperCase() + key.substring(1);
+    }
 
-	public void setShow(int show) {
-		this.show = show;
-	}
+    public Column(String key) {
+        this(key, firstToUpperCase(key), XColumn.type_text, "", XColumn.full, false, false, false);
+    }
+    public Column(String key, XColumn xc, Class<?> jType) {
+        this(key, xc, byJavaType(xc.type(), jType));
+    }
+    public Column(String key, XColumn xc, int xcType) {
+        this(key, XStrings.orElse(xc.value(), firstToUpperCase(key)), xcType, xc.enumKey(), xc.show(), xc.primary(), xc.collapse(), xc.required());
+    }
+    public Column(String key, String hint, int type, String enumKey, int show, boolean primary, boolean collapse, boolean required) {
+        this.key = key;
+        this.hint = hint;
+        this.type = type;
+        this.enumKey = enumKey;
+        this.show = show;
+        this.primary = primary;
+        this.collapse = collapse;
+        this.required = required;
 
-	public int getType() {
-		return type;
-	}
+        if(!XStrings.isEmpty(enumKey) && this.type == 0)
+            this.type = XColumn.type_enum;
+    }
 
-	public void setType(int type) {
-		this.type = type;
-	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
-
-	public String getHint() {
-		return hint;
-	}
-
-	public void setHint(String hint) {
-		this.hint = hint;
-	}
-
-	public String getEnumKey() {
-		return enumKey;
-	}
-
-	public void setEnumKey(String enumKey) {
-		this.enumKey = enumKey;
-	}
-
+    public int getShow() {
+        return show;
+    }
+    public void setShow(int show) {
+        this.show = show;
+    }
+    public int getType() {
+        return type;
+    }
+    public void setType(int type) {
+        this.type = type;
+    }
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+    public String getHint() {
+        return hint;
+    }
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+    public String getEnumKey() {
+        return enumKey;
+    }
+    public void setEnumKey(String enumKey) {
+        this.enumKey = enumKey;
+    }
     public boolean getPrimary() {
         return primary;
     }
-
     public void setPrimary(boolean primary) {
         this.primary = primary;
     }
